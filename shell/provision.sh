32e6e2e3092807d97c0a9e1b4d97ad39b1faa359
Andrei Shevtsov <parad0x.ua@gmail.com>
2014-04-19 12:48:07 -0400

    Updates provision script.

--- shell/provision.sh
+++ shell/provision.sh
@@ -8,7 +8,7 @@ apt-get -y dist-upgrade
 # Remove old rubies, install postgresql git etc
 apt-get remove -y -q ruby1.8 ruby1.8-dev ruby1.9.1 ruby1.9.1-dev
 apt-get install -y -q build-essential libxml2-dev libxslt1-dev libyaml-dev libsqlite3-dev sqlite3 libmysqlclient-dev libpq-dev
-apt-get install -y -q postgresql git
+apt-get install -y -q postgresql git nodejs
 
 # Cleanup
 apt-get autoremove -y
